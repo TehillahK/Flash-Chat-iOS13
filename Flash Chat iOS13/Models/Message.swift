1919155844e7146acbe290f6cb7b0a9a0ad9fbc7
Tehillahk <kangambt@myumanitoba.ca>
2025-02-03 08:50:23 -0600

    created messages struct

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Tehillah Kangamba on 2025-02-03.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    var sender: String
+    var body: String
+}
